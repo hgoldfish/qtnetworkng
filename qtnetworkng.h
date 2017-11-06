24f3b0eb35fbdfb3a38261e8bac48e185b1a3dd6
Qize Huang <hgoldfish@gmail.com>
2017-11-06 13:11:31 +0800

    move qtnetworkng.h to top level directory.

---
+++ qtnetworkng.h
@@ -0,0 +1,11 @@
+#ifndef QTNETWORKNG_H
+#define QTNETWORKNG_H
+
+#include "include/coroutine.h"
+#include "include/locks.h"
+#include "include/eventloop.h"
+#include "include/socket_ng.h"
+#include "include/coroutine_utils.h"
+#include "include/http_ng.h"
+
+#endif // QTNETWORKNG_H
